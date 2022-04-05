ffee3f40c763df03482434229fed661d31a34946
gte1998 <103022999+gtemcmxcviii@users.noreply.github.com>
2022-04-05 13:25:06 +0200

    omg

---
+++ quotes.js
@@ -0,0 +1,196 @@
+export default [
+  {
+    quote:
+      "I have come to love myself for who I am, for who I was, and for who I hope to become",
+    member: "RM",
+  },
+  {
+    quote:
+      "No matter who you are, where you’re from, your skin colour, your gender identity; just speak yourself. Find your name and find your voice by speaking yourself",
+    member: "RM",
+  },
+  {
+    quote: "Teamwork makes the dream work",
+    member: "RM",
+  },
+  {
+    quote:
+      "Life is a sculpture that you cast as you make mistakes and learn from them",
+    member: "RM",
+  },
+  {
+    quote: "Even if you’re not perfect you’re limited edition",
+    member: "RM",
+  },
+  {
+    quote:
+      "Although youth can be beautiful, it can be short and wander off; like a shadow, it has a reckless danger to it",
+    member: "RM",
+  },
+  {
+    quote:
+      "Do you know what purple means? Purple is last color of the rainbow, it means I will trust you and love you for a long time.",
+    member: "V",
+  },
+  {
+    quote: "Your presence can give happiness. I hope you remember that",
+    member: "Jin",
+  },
+  {
+    quote: "I'd rather die than to live without passion",
+    member: "Jungkook",
+  },
+  {
+    quote:
+      "Popularity is a bubble. It’s a mountain: you can go up really hard but walk down really fast",
+    member: "RM",
+  },
+  {
+    quote:
+      "I thought the phrase `Love Yourself` would mean extra special to people who are harsh to themselves, like me for instance",
+    member: "Jimin",
+  },
+  {
+    quote: "Love yourself. Love myself. Peace",
+    member: "Suga",
+  },
+  {
+    quote:
+      "Too many stars, too many dreams. The reality is that in front of these things, I’m just a speck of dust",
+    member: "RM",
+  },
+  {
+    quote: "If you don’t work hard, there won’t be good results",
+    member: "J-Hope",
+  },
+  {
+    quote:
+      "Looking for answers when there aren't any, but we still learn, isn't that what life is about?",
+    member: "RM",
+  },
+  {
+    quote:
+      "It’s hard at times, but it’s as if I’m addicted to basking in your love. When I awake, I want to hurry on stage just for you",
+    member: "J-Hope",
+  },
+  {
+    quote:
+      "I’ve learned to look back and appreciate my times of trial, as they are also what made me",
+    member: "V",
+  },
+  {
+    quote:
+      "We all have lots to pitch in, there’s no knowing what will come. But hard work will get us somewhere",
+    member: "Jungkook",
+  },
+  {
+    quote:
+      "Happiness is not something you have to achieve, you can still feel happy during the process of achieving something",
+    member: "RM",
+  },
+  {
+    quote: "May your trials end in full bloom",
+    member: "Suga",
+  },
+  {
+    quote:
+      "It's okay if you don't have a dream, you might not have one. Just being happy is fine",
+    member: "Suga",
+  },
+  {
+    quote:
+      "There might be turning points in your life. So live to see them through",
+    member: "RM",
+  },
+  {
+    quote: "Those who keep trying without giving up are the ones who succeed",
+    member: "Jin",
+  },
+  {
+    quote:
+      "We have to respect the fact that we all think differently in order to work as one",
+    member: "Suga",
+  },
+  {
+    quote:
+      "What's ordinary to others is special to me whereas what's special to others is very ordinary to me",
+    member: "Suga",
+  },
+  {
+    quote: "You are the leader of your own life",
+    member: "J-Hope",
+  },
+  {
+    quote: "I look forward to the day you will stand and tell your story",
+    member: "V",
+  },
+  {
+    quote: "Keep talking and maybe someday you’ll say something intelligent",
+    member: "Jin",
+  },
+  {
+    quote:
+      "Just because someone else is having a hard time, does not mean your “hard time” is not as difficult. So it is okay to say that you are having a hard time and ask a comfort from people around you.",
+    member: "Jin",
+  },
+  {
+    quote: "If you’re not mad or sad, you won’t know when you are happy",
+    member: "Jungkook",
+  },
+  {
+    quote:
+      "I can’t just tell you to gain strength no matter what, but I do wish that within the hard times you face, you can find things that make you happy",
+    member: "Jungkook",
+  },
+  {
+    quote:
+      "No one knows tomorrow, but today we are the dream we followed yesterday",
+    member: "Suga",
+  },
+  {
+    quote:
+      "In Korean, the word ‘future’ is made up of two parts. The first part means ‘not’ and the second means ‘to come.’ In that sense, ‘future’ means something that will not come. This is to say: The future is now, and our now is us living our future",
+    member: "RM",
+  },
+  {
+    quote:
+      "Someday, if our existence, or our music, our stage, our photos or videos, if all of that can help you even just a little... If your pain is 100 in a scale of 100, if we can lessen that to 99, 98 or 97, if we can only make it like that... the value of our existence is enough",
+    member: "RM",
+  },
+  {
+    quote:
+      "Emotions are so different in every situation and every moment, so I think to agonize every moment is what life is",
+    member: "Suga",
+  },
+  {
+    quote: "I live because I can't die",
+    member: "Suga",
+  },
+  {
+    quote:
+      "We all have the galaxy in our hearts. But there are some people who never find that out until they die",
+    member: "RM",
+  },
+  {
+    quote: "Silence is golden, don't waste time",
+    member: "Jimin",
+  },
+  {
+    quote:
+      "When things get hard, stop for a while and look back and see how far you have come. Don't forget how rewarding it is. You are the most beautiful flower, more than anything else in the world",
+    member: "V",
+  },
+  {
+    quote: "Let’s not push off the things we need to do today to tomorrow",
+    member: "J-Hope",
+  },
+  {
+    quote: "Maybe I made a mistake yesterday but yesterday's me is still me",
+    member: "RM",
+  },
+  {
+    quote:
+      "Effort makes you. You will regret someday if you do not do your best now. Do not think it is too late but keep working on it. It may take time, but there is nothing that gets worse due to practising. So practice. You may get depressed, but its evidence that you are doing good",
+    member: "Jungkook",
+  },
+];
